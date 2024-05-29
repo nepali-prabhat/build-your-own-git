83ddc495b3e357c6eb6b10d41b91b9c082abfd35
Pravat Pandey <pandeypravat@gmail.com>
2024-05-29 12:58:41 +0545

    refactored cat-file command

--- src/main.rs
+++ src/main.rs
@@ -46,11 +46,79 @@ enum ObjectType {
 }
 
 impl ObjectType {
-    fn try_from(s: &str) -> Result<ObjectType, anyhow::Error> {
-        match s {
+    /// Tries to parse a valid object type from the string
+    /// A valid git object is of format:<object_type> <size>\0<content>
+    fn try_from(ot: &str) -> Result<ObjectType, anyhow::Error> {
+        match ot {
             "blob" => Ok(ObjectType::Blob),
-            _ => anyhow::bail!("invalid object type {}", s),
+            _ => anyhow::bail!("invalid object type {}", ot),
         }
+    }
+
+    /// Parses a git object and returns a reader for its decompressed content.
+    /// The object_name doesn't have to be 20 bytes exact; but it has to be unique.
+    fn parse(object_name: String) -> anyhow::Result<(ObjectType, impl Read)> {
+        let objects_dir = Path::new(".git/objects");
+        let dir = fs::read_dir(objects_dir.join(&object_name[..2]))
+            .context("reading object directory")?;
+
+        let matched_objects: Vec<_> = dir
+            .filter(|v| {
+                v.as_ref().map_or(false, |de| {
+                    de.file_name()
+                        .to_str()
+                        .expect("git objects are sha1 hashes")
+                        .to_owned()
+                        .starts_with(&object_name[2..])
+                })
+            })
+            .map(|v| v.unwrap())
+            .collect();
+
+        let total_matched = matched_objects.len();
+
+        if total_matched == 0 {
+            anyhow::bail!("git object {} not found", object_name);
+        }
+        if total_matched > 1 {
+            let objs: Vec<_> = matched_objects
+                .iter()
+                .map(|v| String::from(v.path().to_str().expect("valid sha1 hashes")))
+                .collect();
+            anyhow::bail!(
+                "Found {} objects with the same name: \n{}",
+                objs.len(),
+                objs.join("\n")
+            );
+        }
+
+        let file = fs::File::open(matched_objects[0].path()).context("open in .git/objects")?;
+        let reader = ZlibDecoder::new(file);
+        let mut reader = io::BufReader::new(reader);
+
+        let mut buffer = Vec::new();
+
+        reader
+            .read_until(0, &mut buffer)
+            .context("reading object file")?;
+        buffer.pop(); // removing the null byte
+
+        let s = std::str::from_utf8(&buffer).expect("must be valid utf8");
+
+        let mut s = s.split(" ");
+
+        let ot = s.next().context("parsing object type")?;
+        let obj_type = ObjectType::try_from(ot)?;
+
+        let size = s
+            .next()
+            .context("parsing size")?
+            .parse::<u64>()
+            .context("parsing size as u64")?;
+
+        let reader = reader.take(size);
+
+        Ok((obj_type, reader))
     }
 }
 
@@ -71,76 +139,14 @@ fn main() -> anyhow::Result<()> {
             pretty_print: _,
             object_name,
         } => {
-            let objects_dir = Path::new(".git/objects");
-            let dir = fs::read_dir(objects_dir.join(&object_name[..2]))
-                .context("reading object directory")?;
-
-            let matched_objects: Vec<_> = dir
-                .filter(|v| {
-                    v.as_ref().map_or(false, |de| {
-                        de.file_name()
-                            .to_str()
-                            .expect("git objects are sha1 hashes")
-                            .to_owned()
-                            .starts_with(&object_name[2..])
-                    })
-                })
-                .map(|v| v.unwrap())
-                .collect();
-
-            let total_matched = matched_objects.len();
-
-            if total_matched == 0 {
-                anyhow::bail!("git object {} not found", object_name);
-            }
-            if total_matched > 1 {
-                let objs: Vec<_> = matched_objects
-                    .iter()
-                    .map(|v| String::from(v.path().to_str().expect("valid sha1 hashes")))
-                    .collect();
-                anyhow::bail!(
-                    "Found {} objects with the same name: \n{}",
-                    objs.len(),
-                    objs.join("\n")
-                );
-            }
-
-            let file = fs::File::open(matched_objects[0].path()).context("open in .git/objects")?;
-            let reader = ZlibDecoder::new(&file);
-            let mut reader = io::BufReader::new(reader);
-
-            let mut buffer = Vec::new();
-
-            reader
-                .read_until(0, &mut buffer)
-                .context("reading object file")?;
-            buffer.pop(); // removing the null byte
-
-            let s = std::str::from_utf8(&buffer).expect("must be valid utf8");
-            let mut s = s.split(" ");
-
-            let ot = s.next().context("no object type")?;
-            let obj_type = ObjectType::try_from(ot)?;
+            let (obj_type, mut reader) =
+                ObjectType::parse(object_name).context("parse object type")?;
 
             match obj_type {
                 ObjectType::Blob => {
-                    let n = s.next().context("reading number of bytes")?;
-                    let n = n.parse::<usize>().context("parsing number of bytes")?;
-
-                    // read exactly n number of bytes from the reader;
-                    buffer.clear();
-                    buffer.resize(n, 0);
-
-                    reader
-                        .read_exact(&mut buffer)
-                        .context("reading the contents of the file")?;
-                    let x = reader.read(&mut [0]).expect("ensuring EOF");
-                    anyhow::ensure!(x == 0, "didn't reach EOF");
-
-                    // read exactly n number of bytes from the reader;
                     let stdout = io::stdout();
                     let mut handle = stdout.lock();
-                    handle.write_all(&buffer)?;
+                    io::copy(&mut reader, &mut handle).context("writing contents to stdout")?;
                 }
             }
         }
